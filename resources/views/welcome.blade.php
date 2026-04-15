80c736775d073aa2da1f061c0ef415bb7d50508c
admin <admin@admins-MacBook-Pro.local>
2026-04-15 08:48:11 +0300

    add macro to vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,5 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <img src="{{ Vite::asset('resources/images/cat.jpg') }}">
+        <br>
+        <img src="{{ Vite::image('cat.jpg') }}">
     </body>
 </html>
