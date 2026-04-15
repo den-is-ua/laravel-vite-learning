86dd83d754bb42677cb3e219fbafe4ea5fb5f595
admin <admin@admins-MacBook-Pro.local>
2026-04-15 08:54:29 +0300

    add prefetch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::macro('image', fn ($asset) => $this->asset("resources/images/{$asset}"));
+        Vite::prefetch(concurrency: 3);
     }
 }
