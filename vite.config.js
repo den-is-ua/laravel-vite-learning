392407d4c66305bf912bfffd3dbb9293a22a784b
admin <admin@admins-MacBook-Pro.local>
2026-04-15 09:25:32 +0300

    add manifest SRI

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        manifestSRI(),
     ],
     server: {
         watch: {
